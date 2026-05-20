f1dfa8c1dda626d3ee6acc7faba2f5c13e7cdf80
SHREYAS BS <your.email@example.com>
2026-05-20 21:18:28 +0530

    Add cardiac arrest screen and update manifest

---
+++ app/src/main/kotlin/com/pratham/chikitse/CardiacArrestActivity.kt
@@ -0,0 +1,11 @@
+package com.pratham.chikitse
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CardiacArrestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cardiac_arrest)
+    }
+}
